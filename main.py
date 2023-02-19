ccce901062f44fe38279447678d49bf1fb73a8c1
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-02-19 09:21:44 +0000

    start

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 class Tree:  
   leafs = 569797859686
+  age = 120 
 
+  def __init__(self):
+    print("3434")
 
-
+  def get_older(self):
+    self.age += 1
 
 
 my_tree = Tree()
+
+print(my_tree.age)
+
+my_tree.get_older()
+print(my_tree.age)
